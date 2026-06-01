94c67c7a1411dbbd10ea5208abaa173f8f4647b3
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-06-01 06:26:35 +0000

    Prime Number

--- loops.py
+++ loops.py
@@ -1,1 +1,13 @@
- 
+n = int(input("Enter n:").strip())
+is_prime = True #we assuming 
+if n<=1: #if any user enter 0 or 1 
+    is_prime = False # then it's not a prime
+else:
+    for i in range(2, n): #n or n-1 both can use 
+        if n % i == 0: # n = 17 and i start from 2 so n=17 and i=2 until i = 16 no divisor found 
+            is_prime = False # so this executes 
+            break   # and braks the loop 
+if is_prime: # proving 
+    print(f"{n} is a Prime number ✅")
+else:
+    print(f"{n} is NOT a Prime number ❌")
