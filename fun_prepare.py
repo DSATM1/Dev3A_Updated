67a5e20e1c601fb8a193441719d60a04a4cfb2ba
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-06-03 15:16:31 +0000

    Refactor is_even function examples and comments for clarity

--- fun_prepare.py
+++ fun_prepare.py
@@ -1,1 +1,16 @@
-print("Practicing Functions ")
+#print("Practicing Functions ")
+
+
+
+"""def is_even(n): #type:ignore
+    if n % 2 == 0:
+        return True
+    else:
+        return False    
+res = is_even(1)
+print(res)"""
+
+'''def is_even(n):  #type:ignore
+    return n % 2 == 0  #type:ignore
+
+print(is_even(14))'''
